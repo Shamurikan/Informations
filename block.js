a0b14445886606b504d1fd46f42b04e716b0c869
Shamurikan <107928311+Shamurikan@users.noreply.github.com>
2024-02-25 19:21:14 +0300

    Update block.js

--- block.js
+++ block.js
@@ -2,26 +2,17 @@ alert("start done");
 
 var thisWebsite = window.location;
 
-alert("define done");
+if (thisWebsite.includes("reddit.com")) {
+  alert("site found done");
+  window.location = "https://block.com";
+}
 
-var blocked = [
-  "reddit.com",
-  "youtube.com",
-  "https://www.reddit.com",
-  "",
-  "",
-  "",
-  ""
-];
+if (thisWebsite.includes("youtube.com")) {
+  alert("site found done");
+  window.location = "https://block.com";
+}
 
-alert("define blocked done");
-
-for (block of blocked) {
-  alert("check done");
-  if (thisWebsite.includes(block)) {
-    alert("site found done");
-    window.location = "https://block.com";
-  } else {
-    continue;
-  }
+if (thisWebsite.includes("facebook.com")) {
+  alert("site found done");
+  window.location = "https://block.com";
 }
