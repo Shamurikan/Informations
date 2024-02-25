8b177e06d0a1594cb85606afc38747c9e16f128b
Shamurikan <107928311+Shamurikan@users.noreply.github.com>
2024-02-25 19:24:36 +0300

    Update block.js

--- block.js
+++ block.js
@@ -2,13 +2,7 @@ alert("start done");
 
 var thisWebsite = window.location;
 
-if (thisWebsite.includes("reddit.com")) {
-  alert("site found done");
-  window.location = "https://block.com";
-} else if (thisWebsite.includes("youtube.com")) {
-  alert("site found done");
-  window.location = "https://block.com";
-} else if (thisWebsite.includes("facebook.com")) {
+if (thisWebsite == "https://www.reddit.com") {
   alert("site found done");
   window.location = "https://block.com";
 }
