2daae922eb1216cf33dd939370ccff0756b27307
Shamurikan <107928311+Shamurikan@users.noreply.github.com>
2024-02-25 19:00:47 +0300

    Update block.js

--- block.js
+++ block.js
@@ -1,1 +1,27 @@
+alert("start done");
+
+var thisWebsite = window.location;
+
+alert("define done");
+
+var blocked = [
+  "reddit.com",
+  "youtube.com",
+  "https://www.reddit.com",
+  "",
+  "",
+  "",
+  ""
+];
+
+alert("define blocked done");
+
+for (block of blocked) {
+  alert("check done");
+  if (thisWebsite.includes(block)) {
+    alert("site found done");
     window.location = "https://block.com";
+  } else {
+    continue;
+  }
+}
