a5da2e563118cf0f59fda91601aa1ae3e3aa50a5
Shamurikan <107928311+Shamurikan@users.noreply.github.com>
2024-03-01 20:54:21 +0300

    Update block.js

--- block.js
+++ block.js
@@ -2,8 +2,8 @@ for (i = 0; i < 1000; i++){
   document.getElementsByTagName("ytm-reel-shelf-renderer")[0].remove();
 }
 
-var YouTubeSearchBar = document.getElementsByClassName.("title search-bar-text")[0].innerText;
+var SearchBar = document.getElementsByClassName("title search-bar-text")[0].innerText;
 
-if (YouTubeSearchBar == "new") {
+if (SearchBar == "new") {
   document.body.innerHTML = "";
 }
