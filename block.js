f9ee51289e80d2601bf8059f33e01c7a69f02fe9
Shamurikan <107928311+Shamurikan@users.noreply.github.com>
2024-03-01 20:56:11 +0300

    Update block.js

--- block.js
+++ block.js
@@ -1,7 +1,3 @@
-for (i = 0; i < 1000; i++){
-  document.getElementsByTagName("ytm-reel-shelf-renderer")[0].remove();
-}
-
 var SearchBar = document.getElementsByClassName("title search-bar-text")[0].innerText;
 
 if (SearchBar == "new") {
