3f1181257c7d1b8ebb0b4b866daae3970b1bb881
Shamurikan <107928311+Shamurikan@users.noreply.github.com>
2024-12-25 12:39:12 +0300

    Update ee.js

--- ee.js
+++ ee.js
@@ -1,10 +1,18 @@
-var x = document.getElementByTagName("span")
+var x = document.getElementsByTagName("span");
 
-for( i=0; i< x.length; i++) {
-  if (x[i].innerText = "For you") {
-    console.log("exsit")
-    }
-    console.log(i)
+let found = false; // Flag to check if "For you" exists
+
+for (let i = 0; i < x.length; i++) {
+  if (x[i].innerText === "For you") {
+    console.log("exist");
+    found = true;
+    break; // Exit the loop once found
+  }
+  console.log(i);
 }
-alert("holllllllllla")
-console.log("not exist")
+
+if (!found) {
+  console.log("not exist");
+}
+
+alert("holllllllllla");
