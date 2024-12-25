1e68c4b79d7ec1349c3534fa5441f84e16b24a4d
Shamurikan <107928311+Shamurikan@users.noreply.github.com>
2024-12-25 12:28:28 +0300

    Update ee.js

--- ee.js
+++ ee.js
@@ -1,4 +1,10 @@
+var x = document.getElementByTagName("span")
 
+for( i=0; i< x.length; i++) {
+  if (x[i].innerText = "For you") {
+    console.log("exsit")
+    }
+    console.log(i)
+}
 
-var x = document.getElementByTagName("ytm-search-bar-entry-point-view-model")
-x.style.display = "none";
+console.log("not exist")
